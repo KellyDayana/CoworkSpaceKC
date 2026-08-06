1b12d96d9dd3d20039b9643098b6941a1b5ed90c
Kelly Dayana <kelly.chicaiza5049@utc.edu.ec>
2026-08-06 01:07:10 -0500

    Fix FINAL: Migración vacía 0004 para evitar errores

---
+++ reservas/migrations/0004_no_changes.py
@@ -0,0 +1,11 @@
+# Migración vacía - BD ya correcta
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reservas', '0003_no_changes'),
+    ]
+
+    operations = []
