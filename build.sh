651fc10f7a25504bf2b2f72d865bc3291ad15e61
Kelly Dayana <kelly.chicaiza5049@utc.edu.ec>
2026-08-06 00:51:47 -0500

    Fix: Marcar migraciones como fake para evitar errores con BD existente

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+# Marcar todas las migraciones existentes como aplicadas sin ejecutarlas
+python manage.py migrate --fake
+
+# Aplicar cualquier migración nueva real
 python manage.py migrate
