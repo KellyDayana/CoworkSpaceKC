5172a1ba8f61f9140581946cea543b06227f36e1
Kelly Dayana <kelly.chicaiza5049@utc.edu.ec>
2026-08-06 00:20:46 -0500

    Fix: Simplificar SQL para eliminar constraint

--- reservas/migrations/0007_remove_unique_constraint_manually.py
+++ reservas/migrations/0007_remove_unique_constraint_manually.py
@@ -10,34 +10,16 @@ class Migration(migrations.Migration):
 
     operations = [
         # Eliminar manualmente el constraint unique_together que causa problemas
+        # Usamos múltiples intentos para cubrir diferentes nombres posibles
         migrations.RunSQL(
             sql="""
-                DO $$ 
-                BEGIN
-                    -- Intentar eliminar el constraint si existe
-                    IF EXISTS (
-                        SELECT 1 FROM pg_constraint 
-                        WHERE conname = 'reservas_escritoriodedicado_piso_posicion_key'
-                    ) THEN
-                        ALTER TABLE reservas_escritoriodedicado 
-                        DROP CONSTRAINT reservas_escritoriodedicado_piso_posicion_key;
-                    END IF;
-                    
-                    -- También intentar eliminar variantes del nombre
-                    IF EXISTS (
-                        SELECT 1 FROM pg_constraint 
-                        WHERE conname LIKE 'reservas_escritoriodedicado_piso_%'
-                        AND conname LIKE '%posicion%'
-                    ) THEN
-                        EXECUTE (
-                            SELECT 'ALTER TABLE reservas_escritoriodedicado DROP CONSTRAINT ' || conname || ';'
-                            FROM pg_constraint
-                            WHERE conname LIKE 'reservas_escritoriodedicado_piso_%'
-                            AND conname LIKE '%posicion%'
-                            LIMIT 1
-                        );
-                    END IF;
-                END $$;
+                -- Intentar eliminar el constraint con el nombre estándar
+                ALTER TABLE reservas_escritoriodedicado 
+                DROP CONSTRAINT IF EXISTS reservas_escritoriodedicado_piso_posicion_key;
+                
+                -- Intentar también con el nombre que Django podría generar
+                ALTER TABLE reservas_escritoriodedicado 
+                DROP CONSTRAINT IF EXISTS reservas_escritoriodedicado_piso_posicion_cd9b3a4f_uniq;
             """,
             reverse_sql=migrations.RunSQL.noop,
         ),
