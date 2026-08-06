7301df850158534f9876266e5d989b2fc5371962
Kelly Dayana <kelly.chicaiza5049@utc.edu.ec>
2026-08-06 12:19:01 -0500

    FIX PWA: Service Worker y Manifest servidos correctamente + criterios Google PWA cumplidos

--- CoworkSpaceKC/urls.py
+++ CoworkSpaceKC/urls.py
@@ -1,25 +1,37 @@
 """
 URL configuration for CoworkSpaceKC project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.http import HttpResponse
+import os
+
+# Vista para servir el Service Worker con el Content-Type correcto
+def service_worker(request):
+    sw_path = os.path.join(settings.BASE_DIR, 'static', 'sw.js')
+    with open(sw_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='application/javascript')
+
+# Vista para servir el manifest con el Content-Type correcto
+def manifest(request):
+    manifest_path = os.path.join(settings.BASE_DIR, 'static', 'manifest.json')
+    with open(manifest_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    return HttpResponse(content, content_type='application/manifest+json')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Admin de Django deshabilitado - usar el sistema de gestión web
+    # path('admin/', admin.site.urls),
+    
+    # PWA - Service Worker y Manifest
+    path('sw.js', service_worker, name='service_worker'),
+    path('manifest.json', manifest, name='manifest'),
+    
+    # Rutas principales
     path('', include('reservas.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
